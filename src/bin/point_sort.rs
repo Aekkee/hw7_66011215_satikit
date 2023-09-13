d3469cb77255cdd09e2bc5cb2d03001d807c0806
Aekkee <107569390+Aekkee@users.noreply.github.com>
2023-09-13 17:44:41 +0700

    Update point_sort.rs

--- src/bin/point_sort.rs
+++ src/bin/point_sort.rs
@@ -1,5 +1,5 @@
 fn main() {
-    //2.2
+    //2.1
     let args: Vec<String> = std::env::args().collect();
     let args: Vec<f32> = args[1..].iter().map(|x| x.parse().unwrap_or(0.)).collect();
     if args.is_empty() {
